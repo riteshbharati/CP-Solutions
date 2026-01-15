fa2d186943d9fad59d628098c7cee1a9def63977
Ritesh Bharati <riteshbharati7704@gmail.com>
2026-01-15 20:18:03 +0530

    Add A_Blackslex_and_Password.cpp solution

---
+++ Codeforces/2179/A_Blackslex_and_Password.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define fastio ios::sync_with_stdio(false); cin.tie(nullptr);
+
+typedef long long ll;
+
+int main() {
+    fastio;
+    int t;
+    cin>>t;
+    while(t--){
+        int k,x;
+        cin>>k>>x;
+        cout << (k*x)+1 <<endl;
+    }
+
+    return 0;
+}
